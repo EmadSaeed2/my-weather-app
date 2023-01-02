522c388994303469e3d826f293dfb88a1fc84e6e
Emad Saeed <eaf.saeed@gmail.com>
2023-01-02 19:24:03 +0000

    UPDATE THE UI WITH FIVE DAYS WEATHER

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,63 @@
-var today, lat, lon;
 var apiKey = '318dd89627c7bbdd887f3385d00e0216';
+
 var currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?limit=1&units=metric&appid=${apiKey}&`;
+var fiveDaysWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=${apiKey}&`;
+
+// UPDATE THE UI WITH FIVE DAYS WEATHER
+function updateFiveDaysWeatherUI(fiveDaysData) {
+    console.log('fiveDaysData', fiveDaysData);
+
+    $('.five-days-header').removeClass('display-none');
+    $('.five-days-header').addClass('display-block');
+
+
+    $.each(fiveDaysData.list, function (index, element) {
+        console.log(element)
+        var date = moment(element.dt_txt, 'YYYY-MM-DD hh:mm:ss').format('DD/MM/YYYY');
+        var hour = moment(element.dt_txt, 'YYYY-MM-DD hh:mm:ss').format('ha');
+        var fiveDaysIcon = `http://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png`
+        var fiveDaysTemp = 'Temp: ' + Math.round(element.main.temp) + ' \u2103';
+        var fiveDaysWind = 'Wind: ' + element.wind.speed + ' KPH';
+        var fiveDaysHumidity = 'Humidity: ' + element.main.humidity + '%';
+
+        console.log(date)
+        var fiveDaysWeather = `
+        <div class="five-days-card">
+            <h4 class="card-date">${date}</h4>
+            <h5 class="card-date">${hour}</h5>
+            <img src='${fiveDaysIcon}' alt="">
+            <h5 class="card-weather">${fiveDaysTemp}</h5>
+            <h5 class="card-weather">${fiveDaysWind}</h5>
+            <h5 class="card-weather">${fiveDaysHumidity}</h5>
+        </div>`
+        $('#forecast').append(fiveDaysWeather);
+    });
+
+
+}
+
+// GET 5-DAYS WEATHER DATA
+function getFiveDaysWeatherData(currentData) {
+    if (currentData) {
+        var lon = currentData.coord.lon;
+        var lat = currentData.coord.lat;
+
+        $.get(fiveDaysWeatherUrl + `lat=${lat}&lon=${lon}`)
+            .then(function (fiveDaysData) {
+                updateFiveDaysWeatherUI(fiveDaysData);
+            })
+    }
+
+}
+
+
 
 // UPDATE THE UI WITH THE CURRENT WEATHER
 function updateCurrentWeatherUI(currData) {
-    console.log(currData);
+    // console.log(currData);
 
     // get date with current timezone
-    today = moment().add(currData.timezone, 'seconds').format('DD/MM/YYYY');
+    var today = moment().add(currData.timezone, 'seconds').format('DD/MM/YYYY');
 
     var currCity = currData.name + ' - ' + currData.sys.country;
     var currTemp = 'Temp: ' + Math.round(currData.main.temp) + ' \u2103';
@@ -19,7 +69,7 @@ function updateCurrentWeatherUI(currData) {
     <div class='currWeatherContainer'>
         <div class='d-flex flex-row'>
             <h2 id='currCity'>${currCity} (${today})</h2>
-            <img class='currWeatherIcon' src=' http://openweathermap.org/img/wn/${currWeatherIcon}@2x.png' alt=''>
+            <img class='currWeatherIcon' src='http://openweathermap.org/img/wn/${currWeatherIcon}@2x.png'>
         </div>
         <h4 id='currTemp'>${currTemp}</h4>
         <h4 id='currWind'>${currWind}</h4>
@@ -29,12 +79,13 @@ function updateCurrentWeatherUI(currData) {
     $('#today').html(currentWeather);
 }
 
-// GET CURRENT WEATHER DATA
-function getCurrentWeatherData(cityName) {
+// GET WEATHER DATA
+function getWeatherData(cityName) {
     if (cityName) {
         $.get(`${currentWeatherUrl}q=${cityName}`)
             .then(function (currentData) {
                 updateCurrentWeatherUI(currentData)
+                getFiveDaysWeatherData(currentData)
             });
     } else {
         alert('Please enter a city name.');
@@ -42,14 +93,9 @@ function getCurrentWeatherData(cityName) {
 }
 
 
-
 $('#search-button').click(function (e) {
     e.preventDefault();
     // GET USER INPUT
     var city = $('#search-input').val();
-
-    getCurrentWeatherData(city)
-
-
-
+    getWeatherData(city)
 });
